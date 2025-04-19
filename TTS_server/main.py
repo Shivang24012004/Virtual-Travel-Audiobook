9a86e3b87f0716fe4150c0f9d421d2eb694b1512
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2025-04-19 17:35:05 +0530

    dockerization attempt 1

--- TTS_server/main.py
+++ TTS_server/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://virtual-travel-audiobook-frontend-shivang24012004s-projects.vercel.app"],  # Your React app URL
+    allow_origins=["http://localhost:5173","https://virtual-travel-audiobook-frontend-shivang24012004s-projects.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
